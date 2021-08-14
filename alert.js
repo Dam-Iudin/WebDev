54c7896a41166c4b43f95ad44f19336b478ae194
Dam-pixel <73979404+Dam-pixel@users.noreply.github.com>
2021-08-14 21:11:48 +1200

    try

---
+++ alert.js
@@ -0,0 +1,8 @@
+// document.getElementById("butt").onclick = function() {document.getElementById("demo").innerHTML = "YOU CLICKED ME!"};
+
+// function myFunction() {
+//   document.getElementById("good").innerHTML = "YOU CLICKED ME!";
+// }
+
+
+document.getElementById("butt").onclick = function() {document.getElementById("demo").innerHTML = "Allgood"};
